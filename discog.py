083d6a20d23cfe89aed918a42da3098d52251756
Christophe Thibault <christophe.thibault@telecom-paristech.fr>
2017-11-26 22:26:30 +0100

    test api discogs.com

--- discog.py
+++ discog.py
@@ -1,7 +1,16 @@
 #tests webscraping, API du site www.discogs.com
 import pandas as pd
 import requests
-import BeautifulSoup
+from bs4 import BeautifulSoup
 import numpy as np
+import re
+import discogs_client
+
 
 # Comparaison - recuperation de la discographie soit par scraping soit via l'API du site
+#token hoLtgcULeRfSKyRTtWZdHfVODuXVOJRAbeXoKOtV
+
+#apidiscog = requests.get("https://api.discogs.com/oauth/hoLtgcULeRfSKyRTtWZdHfVODuXVOJRAbeXoKOtV")
+d = discogs_client.Client('ExampleApplication/0.1', user_token="hoLtgcULeRfSKyRTtWZdHfVODuXVOJRAbeXoKOtV")
+results = d.search('Eclipce', type='release')
+print(d.release(8593477))
